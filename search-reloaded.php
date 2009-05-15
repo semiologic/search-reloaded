491eb925f21fab5b53124a4abf4da40a944f8252
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-05-15 15:56:32 +0000

    use dirname() rather than plugin_dir_path()
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/search-reloaded/trunk@1293 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- search-reloaded.php
+++ search-reloaded.php
@@ -18,7 +18,7 @@ http://www.mesoconcepts.com/license/
 **/
 
 
-load_plugin_textdomain('search-reloaded', null, plugin_dir_path(__FILE__) . '/lang');
+load_plugin_textdomain('search-reloaded', null, dirname(__FILE__) . '/lang');
 
 
 /**
